1c7de6eceae281e56423b713b661521f03a9610f
Timothy Roden <95568277+TimGRoden@users.noreply.github.com>
2022-12-01 06:11:27 +0000

    Day1

--- Day1/Day1.cs
+++ Day1/Day1.cs
@@ -11,9 +11,31 @@ namespace Day1
     {
         static void Main(string[] args)
         {
-
-
-
+            List<int> Elves = new List<int>();
+            using (StreamReader sr = new StreamReader(new FileStream("input.txt", FileMode.OpenOrCreate)))
+            {
+                int elf = 0;
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    if (line != "")
+                    {
+                        elf += int.Parse(line);
+                    }
+                    else
+                    {
+                        Elves.Add(elf);
+                        elf = 0;
+                    }
+                }
+            }
+            Console.WriteLine($"An Elf has {Elves.Max()}.");
+            int MaxBerries = Elves.Max();
+            Elves.Remove(Elves.Max());
+            MaxBerries += Elves.Max();
+            Elves.Remove(Elves.Max());
+            MaxBerries += Elves.Max();
+            Console.WriteLine($"Max Berry: {MaxBerries}");
 
             Console.ReadKey();
         }
