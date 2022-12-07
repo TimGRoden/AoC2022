42dea6f5c8ffda9dae5ad9d226403562c991fa97
Timothy Roden <95568277+TimGRoden@users.noreply.github.com>
2022-12-07 07:04:00 +0000

    Day 7

--- Day7/Day7.cs
+++ Day7/Day7.cs
@@ -9,12 +9,42 @@ namespace Day7
 {
     internal class Day7
     {
+        static Directory currFolder;
         static void Main(string[] args)
         {
-
-
-
-
+            string[] filesystem = File.ReadAllLines("input.txt");
+            currFolder = new Directory("/");
+            for (int i=1;i< filesystem.Length; i++) //Start with the first directory in place.
+            { 
+                switch (filesystem[i].Substring(0, 3))
+                {
+                    case "$ c": //Change directory now.
+                        if (filesystem[i].Split(' ')[2] == "..") //Up one, find root.
+                        {
+                            currFolder = currFolder.root;
+                        } else //Go deeper.
+                        {
+                            currFolder = currFolder.directories[currFolder.folderNames.IndexOf(filesystem[i].Split(' ')[2])];
+                        }
+                        break;
+                    case "$ l": //list things.
+                        Console.WriteLine("======================");
+                        currFolder.PrintOut();
+                        break;
+                    default: //Dealing with a file.
+                        currFolder.AddFile(filesystem[i]);
+                        break;
+                }
+            }
+            while (currFolder.root != null) { currFolder = currFolder.root; }//Find the root node.
+            int totalCount = currFolder.countBelow(100000);
+            Console.WriteLine("======================");
+            Console.WriteLine($" / is: {totalCount}");
+            Console.WriteLine("======================");
+            int freeSpace = 70000000 - currFolder.fullSize;
+            int toFree = 30000000 - freeSpace;
+            List<int> directsToDelete = currFolder.listBelow(toFree);
+            Console.WriteLine($"Smallest directory that would work is size {directsToDelete.Min()}.");
             Console.ReadKey();
         }
     }
---
+++ Day7/Directory.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day7
+{
+    internal class Directory
+    {
+        public Directory root;
+        public string folderName;
+        public List<Directory> directories;
+        public List<string> folderNames, fileNames;
+        public int size, fullSize;
+        public List<int> fileSize;
+        public Directory(string folderName)
+        {
+            this.folderName = folderName;
+            size = 0;
+            directories = new List<Directory>();
+            folderNames = new List<string>();
+            fileNames = new List<string>();
+            fileSize = new List<int>();
+        }
+        public void NewFolder(string folderName)
+        {
+            Directory dir = new Directory(folderName);
+            dir.root = this;
+            directories.Add(dir);
+            folderNames.Add(folderName);
+        }
+        public void NewFile(string fileInfo)
+        {
+            string[] data = fileInfo.Split(' ');
+            fileNames.Add(data[1]);
+            fileSize.Add(int.Parse(data[0]));
+            size += int.Parse(data[0]);
+        }
+        public void PrintOut()
+        {
+            Console.WriteLine($"- {folderName} (dir)");
+            foreach (Directory dir in directories)
+            {
+                dir.PrintOut("  ");
+            }
+            for (int i = 0; i < fileNames.Count; i++)
+            {
+                Console.WriteLine($"  - {fileNames[i]} (file, size={fileSize[i]})");
+            }
+        }
+        public void PrintOut(string spaces)
+        {
+            Console.WriteLine($"{spaces}- {folderName} (dir)");
+            foreach (Directory dir in directories)
+            {
+                dir.PrintOut(spaces + "  ");
+            }
+            for (int i = 0; i < fileNames.Count; i++)
+            {
+                Console.WriteLine($"{spaces}  - {fileNames[i]} (file, size={fileSize[i]})");
+            }
+        }
+        public void AddFile(string instr)
+        {
+            if (instr[0] == 'd')
+            {
+                NewFolder(instr.Split(' ')[1]);
+            } else
+            {
+                NewFile(instr);
+            }
+        }
+
+        public int dirSize()
+        {
+            int total = size;
+            foreach (Directory dir in directories)
+            {
+                total += dir.dirSize();
+            }
+            fullSize = total;
+            return total;
+        }
+        public int countBelow(int max)
+        {
+            dirSize();
+            int total = 0;
+            foreach (Directory dir in directories)
+            {
+                dir.dirSize();
+                total += dir.countBelow(max);
+            }
+            if (fullSize <= max) total += fullSize;
+            return total;
+        }
+        public List<int> listBelow(int max)
+        {
+            List<int> below = new List<int>();
+            if (fullSize >= max) below.Add(fullSize); //This file would work.
+            foreach (Directory dir in directories)
+            { //Now check the lower ones, adding their items back on.
+                foreach (int result in dir.listBelow(max))
+                {
+                    below.Add(result);
+                }
+            }
+            return below;
+        }
+    }
+}
