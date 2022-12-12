82c2c42c9ac8e758b28e16a924e8ebfff87c4d4d
Timothy Roden <95568277+TimGRoden@users.noreply.github.com>
2022-12-12 07:52:54 +0000

    Day12

--- Day12/Day12.cs
+++ Day12/Day12.cs
@@ -1,19 +1,142 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
+
 namespace Day12
 {
     internal class Day12
     {
+        static List<Node> MakeNodes(string[] dataIn)
+        {
+            List<Node> Nodes = new List<Node>();
+            for (int y = 0; y < dataIn.Length; y++)
+            {
+                for (int x = 0; x < dataIn[0].Length; x++)
+                {
+                    Node node = new Node(dataIn[y][x], x, y);
+                    Nodes.Add(node);
+                }
+            }
+            //Now nodes exsist, check all connections.
+            for (int y = 0; y < dataIn.Length; y++)
+            {
+                for (int x = 0; x < dataIn[0].Length; x++)
+                { //Current node stored at Nodes[x+y*dataIn[0].Length], since it's contiguous now.
+                    int row = y * dataIn[0].Length; //x+row now
+                    if (x > 0)
+                    {
+                        Nodes[x + row].addNode(Nodes[x + row - 1]); //Look left.
+                    }
+                    if (x < dataIn[0].Length - 1)
+                    {
+                        Nodes[x + row].addNode(Nodes[x + row + 1]); //Look Right
+                    }
+                    if (y > 0)
+                    {
+                        Nodes[x + row].addNode(Nodes[x + row - dataIn[0].Length]); //Look up
+                    }
+                    if (y < dataIn.Length - 1)
+                    {
+                        Nodes[x + row].addNode(Nodes[x + row + dataIn[0].Length]); //Look down.
+                    }
+                }
+            }
+            return Nodes;
+        }
+        static void PrintAll(List<Node> nodes, bool visited)
+        {
+            foreach (Node node in nodes)
+            {
+                if (!visited || visited && node.visited)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    node.printNode();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                    node.printNode();
+                }
+            }
+            Console.WriteLine();
+        }
+        static void AnalyseAll(List<Node> nodes)
+        {
+            foreach (Node node in nodes)
+            {
+                Console.Write($"Node {(char)node.height}. Connected to: ");
+                foreach (Node n in node.connected) Console.Write($"[{(char)n.height}, ({n.name})]");
+                Console.WriteLine();
+            }
+        }
+        static Node nextNode(List<Node> nodes)
+        {
+            Node n = new Node();
+            foreach (Node n1 in nodes)
+            {
+                if (!n1.visited && n1.minDist <= n.minDist) n = n1;
+            }
+            return n;
+        }
+        static int FindE(List<Node> nodes)
+        {
+            Node currNode = nextNode(nodes);
+            //first node found, distace 0.
+            while (nodes.Count > 0)
+            {
+                //Console.WriteLine($"Checking {(char)currNode.height} ({currNode.name})");
+                if (currNode.height == 'E') break; //If you're looking at E as currNode, you're done.
+                foreach (Node neighbor in currNode.connected)
+                { //Look at neighbors.
+                    if (!neighbor.visited) //Not if they've been visited.
+                    {
+                        neighbor.minDist = currNode.minDist + 1; //Set a new minDist.
+                        //Console.WriteLine($"  New distance for {(char)neighbor.height} ({neighbor.name}): {neighbor.minDist}");
+                    }
+                }
+                currNode.visited = true;
+                currNode = nextNode(nodes);
+            }
+            foreach (Node n in nodes) if (n.height == 'E') return n.minDist;
+            return -1;
+        }
+        static int FindA(List<Node> nodes)
+        {
+            Node currNode = new Node();
+            foreach (Node n in nodes)
+            {
+                if (n.height == 'S') n.minDist = int.MaxValue;
+                else if (n.height=='E') currNode = n;
+            }
+            currNode.minDist = 0;
+            while (nodes.Count > 0)
+            {
+                //Console.WriteLine($"Checking {(char)currNode.height} ({currNode.name})");
+                if (currNode.height == 'a') break; //If you're looking at a as currNode, you're done.
+                foreach (Node neighbor in currNode.from)
+                { //Look at neighbors.
+                    if (!neighbor.visited) //Not if they've been visited.
+                    {
+                        neighbor.minDist = currNode.minDist + 1; //Set a new minDist.
+                        //Console.WriteLine($"  New distance for {(char)neighbor.height} ({neighbor.name}): {neighbor.minDist}");
+                    }
+                }
+                currNode.visited = true;
+                currNode = nextNode(nodes);
+            }
+            List<Node> allA = new List<Node>();
+            foreach (Node n in nodes) if (n.height == 'a') allA.Add(n);
+            int steps = int.MaxValue;
+            foreach (Node n in allA) if (n.minDist < steps) steps = n.minDist;
+            return steps;
+        }
         static void Main(string[] args)
         {
-
-
-
-
+            string[] dataIn = File.ReadAllLines("input.txt");
+            List<Node> nodes = MakeNodes(dataIn);
+            Console.WriteLine($"S to E took {FindE(nodes)} steps.");
+            nodes = MakeNodes(dataIn);
+            Console.WriteLine($"E to a took {FindA(nodes)} steps.");
             Console.ReadKey();
         }
     }
---
+++ Day12/Node.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day12
+{
+    internal class Node
+    {
+        public int height, minDist;
+        public List<Node> connected;
+        public List<Node> from;
+        public string name;
+        public bool visited;
+        public Node(char c, int x, int y)
+        {
+            connected = new List<Node>();
+            height = c;
+            name = $"{x},{y}";
+            visited = false;
+            if (c == 'S') minDist = 0;
+            else minDist = int.MaxValue;
+            from = new List<Node>();
+        }
+        public Node()
+        {
+            height = int.MaxValue;
+            minDist = int.MaxValue;
+        }
+        public Node(Node n)
+        {
+            height = n.height;
+            name = n.name;
+            visited = false;
+            connected = new List<Node>();
+            foreach (Node node in n.connected)
+            {
+                connected.Add(node);
+            }
+            minDist = int.MaxValue;
+            from = new List<Node>();
+        }
+        public void addNode(Node n)
+        {
+            if (height == 'S')
+            {
+                connected.Add(n);
+                n.from.Add(this);
+            }
+            else if (!(n.height == 'E') && n.height - height <= 1)
+            {
+                connected.Add(n);
+                n.from.Add(this);
+            }
+            else if (n.height == 'E' && height == 'z') 
+            { 
+                connected.Add(n);
+                n.from.Add(this);
+            }
+        }
+        public void printNode()
+        {
+            Console.SetCursorPosition(int.Parse(name.Split(',')[0]), int.Parse(name.Split(',')[1]));
+            Console.Write((char)height);
+        }
+    }
+}
